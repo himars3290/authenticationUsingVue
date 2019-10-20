614c61513417c9efe78a6b082cbedd171ed55ce3
rashmi <rashmi.manandhar7@gmail.com>
2019-10-20 13:19:56 -0400

    create a register.vue component to register user

---
+++ src/views/Register.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <form action="">
+      <label for="name">
+        Name:
+      </label>
+      <input type="text" name="name" v-model="name" value>
+
+      <label for="email">
+        Email:
+      </label>
+      <input type="text" name="email" v-model="email" value>
+
+      <label for="password">
+        Password:
+      </label>
+      <input type="password" name="password" v-model="password" value>
+
+      <button type="submit" name="button">
+        Register
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: '',
+        email: '',
+        password: ''
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
