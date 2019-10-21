efdced9d8027c80fcc9eb13d521a7ac9069b06db
rashmi <rashmi.manandhar7@gmail.com>
2019-10-21 16:47:48 -0400

    save state on refresh of page

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './vuex/store'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  created() {
+    const userString = localStorage.getItem('user');
+    if (userString) {
+      const userData = JSON.parse(userString);
+      this.$store.commit('SET_USER_DATA', userData);
+    }
+  },
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
